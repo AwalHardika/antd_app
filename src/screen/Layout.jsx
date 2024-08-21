2878ea936e6c0f0217d190ba23ca0811ae2cb47d
AwaHardika <awalrafihardika@gmail.comm>
2024-08-21 08:24:12 +0700

    update lagi

--- src/screen/Layout.jsx
+++ src/screen/Layout.jsx
@@ -1,41 +1,64 @@
 import { Menu } from 'antd'
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import { AiOutlineAudit, AiOutlineDashboard, AiOutlineSetting } from "react-icons/ai"
+import React, { useState } from 'react'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
+import { AiOutlineAudit, AiOutlineDashboard, AiOutlineLogout, AiOutlineSetting, AiOutlineSwapLeft } from "react-icons/ai"
+
 
 const Layout = () => {
+    const navigate = useNavigate()
+    const location = useLocation()
+    const [collapsed, setCollapsed] = useState(false)
     
     const items =[
         {
+            icon : <AiOutlineSwapLeft/>,
+            onClick : ()=>{
+                setCollapsed(prev=> prev=!prev)
+            }
+        },
+        {
             key : '/',
             label : "Dashboard",
-            icon : <AiOutlineDashboard/>
+            icon : <AiOutlineDashboard/>,
+            onClick : ()=>{
+                navigate("/")
+            }
         },
         {
             key : '/mahasiswa',
             label : 'Data Mahasiswa',
-            icon : <AiOutlineAudit/>
+            icon : <AiOutlineAudit/>,
+            onClick : ()=>{
+                navigate("/mahasiswa")
+            }
         },
         {
             key : '/settings',
             label : 'Settings',
-            icon : <AiOutlineSetting/>
-        }
+            icon : <AiOutlineSetting/>,
+            onClick : ()=>{
+                navigate("/settings")
+            }
+        },
+   
     ]
     return (
     <main className='flex relative'>
         {/* sidebar */}
-        <div className='w-[200px] h-auto'>
+        <div className='w-[200px] h-auto fixed'>
+        
         <Menu 
+        defaultSelectedKeys={[location.pathname]}
         mode='inline'
         items={items}
-        className='h-screen'
+        className='h-screen relative'
         theme='dark'
+        inlineCollapsed= {collapsed}
         />
         </div>
 
         {/* right content */}
-        <div className='flex-1'>
+        <div className='flex-1 h-[2000px] ml-[200px]'>
         <Outlet/>
         </div>
         
